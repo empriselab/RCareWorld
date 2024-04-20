40ebce9a6a22b0ee92c45ba091c8a141bb8ec9b8
YoruCathy <cathyye0809@gmail.com>
2024-04-20 17:58:21 -0400

    exe path

--- pyrcareworld/Examples/example_load_object.py
+++ pyrcareworld/Examples/example_load_object.py
@@ -1,6 +1,6 @@
 from pyrcareworld.envs import RCareWorld
 
-env = RCareWorld(assets=["Cube"])
+env = RCareWorld(executable_file="your_pth.x86_64", assets=["Cube"])
 cube = env.create_object(id=12345, name="Cube", is_in_scene=False)
 cube.load()
 for i in range(500):
