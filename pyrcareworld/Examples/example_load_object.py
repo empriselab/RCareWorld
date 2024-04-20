4753de6f8ec8e05c0a729ec62ddf3d8baea526ae
YoruCathy <cathyye0809@gmail.com>
2024-04-20 18:33:53 -0400

    add mac

--- pyrcareworld/Examples/example_load_object.py
+++ pyrcareworld/Examples/example_load_object.py
@@ -1,6 +1,7 @@
 from pyrcareworld.envs import RCareWorld
 
 env = RCareWorld(executable_file="your_pth.x86_64", assets=["Cube"])
+# if mac, something like /home/cathy/Workspace/RCareUnity/Build/TestInstall/Mac/loadObject.app/Contents/MacOS/RCareWorld
 cube = env.create_object(id=12345, name="Cube", is_in_scene=False)
 cube.load()
 for i in range(500):
