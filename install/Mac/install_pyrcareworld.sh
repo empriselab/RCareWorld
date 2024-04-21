2f11154e58ac45be5887f1d0d8c1c698a03249e9
YoruCathy <cathyye0809@gmail.com>
2024-04-21 15:02:13 -0400

    install guide

---
+++ install/Mac/install_pyrcareworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ~/miniconda3/etc/profile.d/conda.sh
+conda create -y --name rcareworld python=3.8 pip
+conda activate rcareworld
+cd pyrcareworld
+pip install -r requirements.txt
+pip install -e .
+cd ..
+
+brew install openssl
+export LDFLAGS=“-L/opt/homebrew/opt/openssl@3/lib”
+export CPPFLAGS=“-I/opt/homebrew/opt/openssl@3/include”
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
+
+brew install libomp
