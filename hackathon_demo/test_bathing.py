46fac903b46acc53892bc08d5bb13de382573a4c
thisjustin123 <jjg283@cornell.edu>
2024-04-21 21:01:53 -0400

    Add force and paint proportion data

--- hackathon_demo/test_bathing.py
+++ hackathon_demo/test_bathing.py
@@ -1,15 +1,44 @@
 from pyrcareworld.envs import RCareWorld
+from pyrcareworld.attributes import sponge_attr
 import random
 
-env = RCareWorld()
-robot = env.create_robot(
-    id=12345, gripper_list=["123450"], robot_name="stretch3", base_pos=[0, 0, 0]
-)
-target = env.create_object(id=2333, name="Cube", is_in_scene=True)
-for i in range(10):
-    env.step()
-while True:
-    position = target.getPosition()
-    rotation = target.getRotation()
-    robot.directlyMoveTo(position)
-    env.step()
+# Script to test bed bathing in hackathon.
+if __name__ == "__main__":
+    env = RCareWorld()
+
+    # A collection of all force readings over all steps.
+    all_nonzero_forces = []
+
+    # robot = env.create_robot(
+    #     id=12345, gripper_list=["123450"], robot_name="stretch3", base_pos=[0, 0, 0]
+    # )
+    target = env.create_object(id=2333, name="Cube", is_in_scene=True)
+    for i in range(10):
+        env.step()
+    while True:
+        position = target.getPosition()
+        rotation = target.getRotation()
+        # robot.directlyMoveTo(position)
+
+        # All new forces, assumed to be non-zero by Unity.
+        forces = env.instance_channel.data[509]["forces"]
+        # New proportion, or None if not updated.
+        prop = env.instance_channel.data[509]["proportion"]
+        msg = ""
+        if len(forces) > 0:
+            for force in forces:
+                all_nonzero_forces.append(force)
+
+            msg += (
+                "Forces Score: "
+                + str(sponge_attr.score_forces(all_nonzero_forces))
+                + "\n"
+            )
+
+        if prop != None:
+            msg += "Bathing Score: " + str(prop) + "\n"
+        if len(msg) > 0:
+            msg += "*************"
+            print(msg)
+
+        env.step()
--- pyrcareworld/pyrcareworld/attributes/sponge_attr.py
+++ pyrcareworld/pyrcareworld/attributes/sponge_attr.py
@@ -9,10 +9,35 @@ import pyrcareworld.utils.utility as utility
 def parse_message(msg: IncomingMessage) -> dict:
     """
     Adds a new dictionary entry `"forces"` that contains the forces acting on the sponge. May contain no forces if the sponge is not currently being pushed.
+
+    Also populates `"proportion"` with the proportion of the texture that is painted, or None if no proportion is available.
     """
     this_object_data = attr.base_attr.parse_message(msg)
     count = msg.read_int32()
     this_object_data["forces"] = []
     if count > 0:
         this_object_data["forces"] = msg.read_float32_list()
+    has_proportion = msg.read_bool()
+    this_object_data["proportion"] = None
+    if has_proportion:
+        this_object_data["proportion"] = msg.read_float32()
     return this_object_data
+
+
+def score_forces(forces: list) -> float:
+    """
+    Given a list of forces, returns a score for it between 0 and 1, 1 being
+    perfect. If an empty list is input, 0 is returned.
+    """
+    if len(forces) == 0:
+        return 0
+    # Currently implemented with a simple heuristic that returns the
+    # proportion of forces in a threshold.
+    lower = 0
+    upper = 200
+
+    num_within = 0
+    for force in forces:
+        if force > lower and force < upper:
+            num_within += 1
+    return num_within / len(forces)
