d859ccccb8b8dbdd5806781974796c6ab04cb6ce
YoruCathy <cathyye0809@gmail.com>
2024-06-30 19:55:51 -0400

    Removed conda installation script.

--- install/Mac/install_conda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Ensuring the directory for Miniconda installation exists
-mkdir -p ~/miniconda3
-curl -L "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh" -o ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
