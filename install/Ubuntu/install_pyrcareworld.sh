285d0576782891dfe4898ec23c349ad060d57c2e
YoruCathy <cathyye0809@gmail.com>
2024-07-26 11:01:01 -0400

    Updated readme.

--- install/Ubuntu/install_pyrcareworld.sh
+++ install/Ubuntu/install_pyrcareworld.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -l
 eval "$(conda shell.bash hook)"
-conda create -y --name rcareworld python=3.8 pip
+conda create -y --name rcareworld python=3.10 pip
 source activate rcareworld
 cd pyrcareworld
 pip install -r requirements.txt
