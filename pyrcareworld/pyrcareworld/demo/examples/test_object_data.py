68da5f2a7aef639aad5ea04a95113331a3012e71
Tom Silver <tomssilver@gmail.com>
2024-07-29 14:45:49 -0400

    object data

--- pyrcareworld/pyrcareworld/demo/examples/test_object_data.py
+++ pyrcareworld/pyrcareworld/demo/examples/test_object_data.py
@@ -1,21 +1,23 @@
 from pyrcareworld.envs.base_env import RCareWorld
 import pyrcareworld.attributes as attr
 
-env = RCareWorld(assets=["Rigidbody_Box", "franka_panda"])
+def test_object_data():
+    """Tests accessing robot data."""
+    env = RCareWorld(assets=["Rigidbody_Box", "franka_panda"], graphics=False)
 
-box = env.InstanceObject(name="Rigidbody_Box", id=123456, attr_type=attr.RigidbodyAttr)
-box.SetTransform(position=[0, 1, 0])
-env.step(5)
-for key in box.data:
-    print(key)
-    print(box.data[key])
+    box = env.InstanceObject(name="Rigidbody_Box", id=123456, attr_type=attr.RigidbodyAttr)
+    box.SetTransform(position=[0, 1, 0])
+    env.step(5)
+    for key in box.data:
+        print(key)
+        print(box.data[key])
 
-robot = env.InstanceObject(
-    name="franka_panda", id=789789, attr_type=attr.ControllerAttr
-)
-robot.SetTransform(position=[1, 0, 0])
-env.step()
-for key in robot.data:
-    print(key)
-    print(robot.data[key])
-env.close()
+    robot = env.InstanceObject(
+        name="franka_panda", id=789789, attr_type=attr.ControllerAttr
+    )
+    robot.SetTransform(position=[1, 0, 0])
+    env.step()
+    for key in robot.data:
+        print(key)
+        print(robot.data[key])
+    env.close()
