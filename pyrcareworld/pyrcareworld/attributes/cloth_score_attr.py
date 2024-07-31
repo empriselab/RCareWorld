dff562d1f5b621febeaf0f41c9d4cb4ac93d61e6
Alkdis Chen <alkdischen@gmail.com>
2024-07-31 10:29:10 -0400

    Update ClothScore & SpongeAttr

--- pyrcareworld/pyrcareworld/attributes/cloth_score_attr.py
+++ pyrcareworld/pyrcareworld/attributes/cloth_score_attr.py
@@ -5,71 +5,160 @@ from pyrcareworld.side_channel.side_channel import (
 )
 import pyrcareworld.utils.utility as utility
 
-
 def parse_message(msg: IncomingMessage) -> dict:
     """
-    Writes 8 new entries to the dictionary:
-
-    `[left or right]_[upper or fore]arm_hole_likelihood`, corresponding to the score rating for how likely it is that the arm is actually in the hole.
-
-    `[left or right]_[upper or fore]arm_distance`, corresponding to the distance between the arm and the hole. -1 corresponds to the hole being just on the entering edge of the arm. 1 corresponds to it being just on the exit edge of the arm. Not clamped.
-
-    Returns the modified dictionary.
+    Parses messages received from Unity with more detailed metrics and additional data points.
     """
     this_object_data = attr.base_attr.parse_message(msg)
 
-    # Get scoring info
+    # Get detailed scoring info
     right_forearm_hole_likelihood = msg.read_float32()
     right_upperarm_hole_likelihood = msg.read_float32()
     left_forearm_hole_likelihood = msg.read_float32()
     left_upperarm_hole_likelihood = msg.read_float32()
 
-    this_object_data["right_forearm_hole_likelihood"] = right_forearm_hole_likelihood
-    this_object_data["right_upperarm_hole_likelihood"] = right_upperarm_hole_likelihood
-    this_object_data["left_forearm_hole_likelihood"] = left_forearm_hole_likelihood
-    this_object_data["left_upperarm_hole_likelihood"] = left_upperarm_hole_likelihood
+    # Populate the dictionary with hole likelihood scores
+    this_object_data.update({
+        "right_forearm_hole_likelihood": right_forearm_hole_likelihood,
+        "right_upperarm_hole_likelihood": right_upperarm_hole_likelihood,
+        "left_forearm_hole_likelihood": left_forearm_hole_likelihood,
+        "left_upperarm_hole_likelihood": left_upperarm_hole_likelihood
+    })
 
+    # Read distance metrics
     right_forearm_distance = msg.read_float32()
     right_upperarm_distance = msg.read_float32()
     left_forearm_distance = msg.read_float32()
     left_upperarm_distance = msg.read_float32()
 
-    this_object_data["right_forearm_distance"] = right_forearm_distance
-    this_object_data["right_upperarm_distance"] = right_upperarm_distance
-    this_object_data["left_forearm_distance"] = left_forearm_distance
-    this_object_data["left_upperarm_distance"] = left_upperarm_distance
+    # Populate the dictionary with distance scores
+    this_object_data.update({
+        "right_forearm_distance": right_forearm_distance,
+        "right_upperarm_distance": right_upperarm_distance,
+        "left_forearm_distance": left_forearm_distance,
+        "left_upperarm_distance": left_upperarm_distance
+    })
+
+    # New data points for shoulder and overall arm coverage
+    shoulder_coverage = msg.read_float32()
+    right_arm_coverage = msg.read_float32()
+    left_arm_coverage = msg.read_float32()
+
+    # Populate the dictionary with coverage scores
+    this_object_data.update({
+        "shoulder_coverage": shoulder_coverage,
+        "right_arm_coverage": right_arm_coverage,
+        "left_arm_coverage": left_arm_coverage
+    })
 
     return this_object_data
 
-
 def score_forces(data: dict) -> float:
     """
-    Scores a set of data in the form of a dictionary returned by parse_message.
-
-    Feel free to change.
+    Revised scoring mechanism that factors in both distance and hole likelihood, 
+    and adds consideration for shoulder and overall arm coverage.
     """
-    left_arm_distance_score = 0
-    right_arm_distance_score = 0
-    if data["left_forearm_distance"] > -1 and data["left_forearm_distance"] < 1:
-        left_arm_distance_score = (data["left_forearm_distance"] + 1) / 2
-    elif data["left_upperarm_distance"] > -1 and data["left_upperarm_distance"] < 1:
-        left_arm_distance_score = (data["left_upperarm_distance"] + 1) / 2
-        left_arm_distance_score += 1
+    # Calculate distance scores considering both forearm and upper arm
+    left_arm_distance_score = utility.calculate_score(data["left_forearm_distance"], data["left_upperarm_distance"])
+    right_arm_distance_score = utility.calculate_score(data["right_forearm_distance"], data["right_upperarm_distance"])
 
-    if data["right_forearm_distance"] > -1 and data["right_forearm_distance"] < 1:
-        right_arm_distance_score = (data["right_forearm_distance"] + 1) / 2
-    elif data["right_upperarm_distance"] > -1 and data["right_upperarm_distance"] < 1:
-        right_arm_distance_score = (data["right_upperarm_distance"] + 1) / 2
-        right_arm_distance_score += 1
+    # Calculate the maximum likelihood for each arm being in the hole
+    left_likelihood = max(data["left_forearm_hole_likelihood"], data["left_upperarm_hole_likelihood"])
+    right_likelihood = max(data["right_forearm_hole_likelihood"], data["right_upperarm_hole_likelihood"])
 
-    left_likelihood = max(
-        data["left_forearm_hole_likelihood"], data["left_upperarm_hole_likelihood"]
-    )
-    right_likelihood = max(
-        data["right_forearm_hole_likelihood"], data["right_upperarm_hole_likelihood"]
+    # Calculate the coverage score, factoring in both shoulders and arms
+    coverage_score = data["shoulder_coverage"] + data["right_arm_coverage"] + data["left_arm_coverage"]
+
+    # Calculate the overall score
+    overall_score = (
+        left_arm_distance_score * left_likelihood +
+        right_arm_distance_score * right_likelihood +
+        coverage_score
     )
 
-    return (
-        left_arm_distance_score * left_likelihood
-        + right_arm_distance_score * right_likelihood
-    )
+    return overall_score
+
+if __name__ == "__main__":
+    # Example usage
+    msg = IncomingMessage(data=b'Your binary data here')
+    data_dict = parse_message(msg)
+    score = score_forces(data_dict)
+    print("Calculated Score:", score)
+
+
+
+# import pyrcareworld.attributes as attr
+# from pyrcareworld.side_channel.side_channel import (
+#     IncomingMessage,
+#     OutgoingMessage,
+# )
+# import pyrcareworld.utils.utility as utility
+
+
+# def parse_message(msg: IncomingMessage) -> dict:
+#     """
+#     Writes 8 new entries to the dictionary:
+
+#     `[left or right]_[upper or fore]arm_hole_likelihood`, corresponding to the score rating for how likely it is that the arm is actually in the hole.
+
+#     `[left or right]_[upper or fore]arm_distance`, corresponding to the distance between the arm and the hole. -1 corresponds to the hole being just on the entering edge of the arm. 1 corresponds to it being just on the exit edge of the arm. Not clamped.
+
+#     Returns the modified dictionary.
+#     """
+#     this_object_data = attr.base_attr.parse_message(msg)
+
+#     # Get scoring info
+#     right_forearm_hole_likelihood = msg.read_float32()
+#     right_upperarm_hole_likelihood = msg.read_float32()
+#     left_forearm_hole_likelihood = msg.read_float32()
+#     left_upperarm_hole_likelihood = msg.read_float32()
+
+#     this_object_data["right_forearm_hole_likelihood"] = right_forearm_hole_likelihood
+#     this_object_data["right_upperarm_hole_likelihood"] = right_upperarm_hole_likelihood
+#     this_object_data["left_forearm_hole_likelihood"] = left_forearm_hole_likelihood
+#     this_object_data["left_upperarm_hole_likelihood"] = left_upperarm_hole_likelihood
+
+#     right_forearm_distance = msg.read_float32()
+#     right_upperarm_distance = msg.read_float32()
+#     left_forearm_distance = msg.read_float32()
+#     left_upperarm_distance = msg.read_float32()
+
+#     this_object_data["right_forearm_distance"] = right_forearm_distance
+#     this_object_data["right_upperarm_distance"] = right_upperarm_distance
+#     this_object_data["left_forearm_distance"] = left_forearm_distance
+#     this_object_data["left_upperarm_distance"] = left_upperarm_distance
+
+#     return this_object_data
+
+
+# def score_forces(data: dict) -> float:
+#     """
+#     Scores a set of data in the form of a dictionary returned by parse_message.
+
+#     Feel free to change.
+#     """
+#     left_arm_distance_score = 0
+#     right_arm_distance_score = 0
+#     if data["left_forearm_distance"] > -1 and data["left_forearm_distance"] < 1:
+#         left_arm_distance_score = (data["left_forearm_distance"] + 1) / 2
+#     elif data["left_upperarm_distance"] > -1 and data["left_upperarm_distance"] < 1:
+#         left_arm_distance_score = (data["left_upperarm_distance"] + 1) / 2
+#         left_arm_distance_score += 1
+
+#     if data["right_forearm_distance"] > -1 and data["right_forearm_distance"] < 1:
+#         right_arm_distance_score = (data["right_forearm_distance"] + 1) / 2
+#     elif data["right_upperarm_distance"] > -1 and data["right_upperarm_distance"] < 1:
+#         right_arm_distance_score = (data["right_upperarm_distance"] + 1) / 2
+#         right_arm_distance_score += 1
+
+#     left_likelihood = max(
+#         data["left_forearm_hole_likelihood"], data["left_upperarm_hole_likelihood"]
+#     )
+#     right_likelihood = max(
+#         data["right_forearm_hole_likelihood"], data["right_upperarm_hole_likelihood"]
+#     )
+
+#     return (
+#         left_arm_distance_score * left_likelihood
+#         + right_arm_distance_score * right_likelihood
+#     )
--- pyrcareworld/pyrcareworld/attributes/sponge_attr.py
+++ pyrcareworld/pyrcareworld/attributes/sponge_attr.py
@@ -1,26 +1,26 @@
 import pyrcareworld.attributes as attr
-from pyrcareworld.side_channel.side_channel import (
-    IncomingMessage,
-    OutgoingMessage,
-)
+from pyrcareworld.side_channel.side_channel import IncomingMessage, OutgoingMessage
 import pyrcareworld.utils.utility as utility
 
-
 def parse_message(msg: IncomingMessage) -> dict:
-    """
-    Adds a new dictionary entry `"forces"` that contains the forces acting on the sponge. May contain no forces if the sponge is not currently being pushed.
 
-    Also populates `"proportion"` with the proportion of the texture that is painted, or None if no proportion is available.
-    """
     this_object_data = attr.base_attr.parse_message(msg)
-    count = msg.read_int32()
-    this_object_data["forces"] = []
-    if count > 0:
-        this_object_data["forces"] = msg.read_float32_list()
-    has_proportion = msg.read_bool()
-    this_object_data["proportion"] = None
-    if has_proportion:
-        this_object_data["proportion"] = msg.read_float32()
+    
+    is_grasped = msg.read_bool()
+    this_object_data['is_grasped'] = is_grasped
+
+    is_sponge_in_water = msg.read_bool()
+    this_object_data['is_sponge_in_water'] = is_sponge_in_water
+
+    is_sponge_touching_manikin = msg.read_bool()
+    this_object_data['is_sponge_touching_manikin'] = is_sponge_touching_manikin
+
+    force_during_contact = msg.read_float32()
+    this_object_data['force_during_contact'] = force_during_contact
+
+    full_body_bathing_score = msg.read_int32()
+    this_object_data['full_body_bathing_score'] = full_body_bathing_score
+
     return this_object_data
 
 
@@ -41,3 +41,69 @@ def score_forces(forces: list) -> float:
         if force > lower and force < upper:
             num_within += 1
     return num_within / len(forces)
+
+
+
+def calculate_scores(data: dict) -> None:
+    """
+    Calculates scores based on data received from Unity.
+    """
+    if data['is_grasped']:
+        print("Scrubber successfully grasped, score +5")
+    if data['is_sponge_in_water']:
+        print("Scrubber successfully dipped in water, score +5")
+    if data['is_sponge_touching_manikin']:
+        print("Scrubber is touching the manikin, calculating force and coverage scores.")
+        print(f"Contact force: {data['force_during_contact']}N")
+        print(f"Full-body bed bathing score: {data['full_body_bathing_score']}")
+
+
+if __name__ == "__main__":
+    example_msg = IncomingMessage(data=b'Binary data representing the message')
+    parsed_data = parse_message(example_msg)
+    calculate_scores(parsed_data)
+
+
+# import pyrcareworld.attributes as attr
+# from pyrcareworld.side_channel.side_channel import (
+#     IncomingMessage,
+#     OutgoingMessage,
+# )
+# import pyrcareworld.utils.utility as utility
+
+
+# def parse_message(msg: IncomingMessage) -> dict:
+#     """
+#     Adds a new dictionary entry `"forces"` that contains the forces acting on the sponge. May contain no forces if the sponge is not currently being pushed.
+
+#     Also populates `"proportion"` with the proportion of the texture that is painted, or None if no proportion is available.
+#     """
+#     this_object_data = attr.base_attr.parse_message(msg)
+#     count = msg.read_int32()
+#     this_object_data["forces"] = []
+#     if count > 0:
+#         this_object_data["forces"] = msg.read_float32_list()
+#     has_proportion = msg.read_bool()
+#     this_object_data["proportion"] = None
+#     if has_proportion:
+#         this_object_data["proportion"] = msg.read_float32()
+#     return this_object_data
+
+
+# def score_forces(forces: list) -> float:
+#     """
+#     Given a list of forces, returns a score for it between 0 and 1, 1 being
+#     perfect. If an empty list is input, 0 is returned.
+#     """
+#     if len(forces) == 0:
+#         return 0
+#     # Currently implemented with a simple heuristic that returns the
+#     # proportion of forces in a threshold.
+#     lower = 0
+#     upper = 200
+
+#     num_within = 0
+#     for force in forces:
+#         if force > lower and force < upper:
+#             num_within += 1
+#     return num_within / len(forces)
