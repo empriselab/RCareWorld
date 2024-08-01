69e3ad528176f3a11ce4c5f475f469fff89b9a52
Alkdis Chen <alkdischen@gmail.com>
2024-08-01 13:25:15 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../pyrcareworld'))
 project = 'PhyRC Challenge'
 copyright = '2024, EmPRISE Lab'
 author = 'EmPRISE Lab'
-release = '1.0.1'
+release = '1.5.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
