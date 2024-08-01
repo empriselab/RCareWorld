a571e4a3c6f8019ffed9f4b44dd9ae03e4207ab9
YoruCathy <cathyye0809@gmail.com>
2024-07-31 22:54:46 -0400

    Reformatted tactile sensor comments.

--- pyrcareworld/pyrcareworld/attributes/digit_attr.py
+++ pyrcareworld/pyrcareworld/attributes/digit_attr.py
@@ -1,22 +1,22 @@
 import base64
 import pyrcareworld.attributes as attr
 
-
 class DigitAttr(attr.BaseAttr):
     """
     Class for simulating DIGIT tactile sensor.
+    https://digit.ml/
     """
 
     def parse_message(self, data: dict):
         """
-        Parse messages. This function is called by internal function.
+        Parse messages. This function is called by an internal function.
 
-        Returns:
-            Dict: A dict containing useful information of this class.
+        :param data: Dictionary containing the message data.
+        :return: A dict containing useful information of this class.
+        :rtype: dict
 
-            self.data['light']: Bytes of RGB light image in DIGIT.
-
-            self.data['depth']: Bytes of depth image in DIGIT.
+        self.data['light']: Bytes of RGB light image in DIGIT.
+        self.data['depth']: Bytes of depth image in DIGIT.
         """
         super().parse_message(data)
         if "light" in self.data:
@@ -27,6 +27,5 @@ class DigitAttr(attr.BaseAttr):
     def GetData(self):
         """
         Get data from DIGIT.
-
         """
         self._send_data("GetData")
