6411a7b753baac611251262761db6fc9dedbd90a
YoruCathy <cathyye0809@gmail.com>
2024-07-31 22:57:56 -0400

    Reformatted light attr comments.

--- pyrcareworld/pyrcareworld/attributes/light_attr.py
+++ pyrcareworld/pyrcareworld/attributes/light_attr.py
@@ -1,28 +1,23 @@
 from enum import Enum
 import pyrcareworld.attributes as attr
 
-
 class LightType(Enum):
     """
-    The type of light, keeping same name with LightType (https://docs.unity3d.com/ScriptReference/LightType.html) in Unity.
+    The type of light, keeping the same name as LightType (https://docs.unity3d.com/ScriptReference/LightType.html) in Unity.
     """
-
     Spot = 0
     Directional = 1
     Point = 2
     Area = 3  # unused
     Disc = 4  # unused
 
-
 class LightShadow(Enum):
     """
-    The type of shadow, keeping same name with LightShadows (https://docs.unity3d.com/ScriptReference/LightShadows.html) in Unity.
+    The type of shadow, keeping the same name as LightShadows (https://docs.unity3d.com/ScriptReference/LightShadows.html) in Unity.
     """
-
     NoneShadow = 0
     Hard = 1
     Soft = 2
-
 
 class LightAttr(attr.BaseAttr):
     """
@@ -31,21 +26,21 @@ class LightAttr(attr.BaseAttr):
 
     def parse_message(self, data: dict):
         """
-        Parse messages. This function is called by internal function.
+        Parse messages. This function is called by an internal function.
 
-        Returns:
-            Dict: A dict containing useful information of this class.
+        :param data: Dictionary containing the message data.
+        :return: A dict containing useful information of this class.
+        :rtype: dict
         """
         super().parse_message(data)
 
     def SetColor(self, color: list):
         """
-        Set the color of light.
+        Set the color of the light.
 
-        Args:
-            color: A list of length 3, representing the R, G and B channel, in range [0, 1].
+        :param color: A list of length 3, representing the R, G, and B channels, in range [0, 1].
         """
-        assert color is not None and len(color) == 3, "position length must be 3"
+        assert color is not None and len(color) == 3, "color length must be 3"
         color = [float(i) for i in color]
 
         self._send_data("SetColor", color)
@@ -54,8 +49,7 @@ class LightAttr(attr.BaseAttr):
         """
         Set the type of light.
 
-        Args:
-            light_type: LightType, the type of light.
+        :param light_type: LightType, the type of light.
         """
         self._send_data("SetType", light_type.value)
 
@@ -63,34 +57,30 @@ class LightAttr(attr.BaseAttr):
         """
         Set the type of shadow.
 
-        Args:
-            light_shadow: LightShadow, the type of the shadow.
+        :param light_shadow: LightShadow, the type of shadow.
         """
         self._send_data("SetShadow", light_shadow.value)
 
     def SetIntensity(self, light_intensity: float):
         """
-        Set the intensity of light.
+        Set the intensity of the light.
 
-        Args:
-            light_intensity: Float, the intensity of light.
+        :param light_intensity: Float, the intensity of the light.
         """
         self._send_data("SetIntensity", float(light_intensity))
 
     def SetRange(self, light_range: float):
         """
-        Set the range of light. (Only available when the LightType is `LightType.Spot` or `LightType.Point`)
+        Set the range of the light. (Only available when the LightType is `LightType.Spot` or `LightType.Point`)
 
-        Args:
-            light_range: Float, the range of light.
+        :param light_range: Float, the range of the light.
         """
         self._send_data("SetRange", float(light_range))
 
     def SetSpotAngle(self, spot_angle: float):
         """
-        Set the angle of light. (Only available when the LightType is `LightType.Spot`)
+        Set the angle of the light. (Only available when the LightType is `LightType.Spot`)
 
-        Args:
-            spot_angle: Float, the angle of light.
+        :param spot_angle: Float, the angle of the light.
         """
         self._send_data("SetSpotAngle", float(spot_angle))
