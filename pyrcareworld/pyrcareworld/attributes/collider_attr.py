34bdcf3d7b838746a1597ccfba937a53e918f4b8
YoruCathy <cathyye0809@gmail.com>
2024-07-31 22:54:00 -0400

    Reformatted collider attr comments.

--- pyrcareworld/pyrcareworld/attributes/collider_attr.py
+++ pyrcareworld/pyrcareworld/attributes/collider_attr.py
@@ -1,62 +1,45 @@
 import pyrcareworld.attributes as attr
 
-
 class ColliderAttr(attr.GameObjectAttr):
     """
-    Collider class for objects who have collider in Unity.
+    Collider class for objects that have a collider in Unity.
     """
 
     def parse_message(self, data: dict):
         """
-        Parse messages. This function is called by internal function.
+        Parse messages. This function is called by an internal function.
 
-        Returns:
-            Dict: A dict containing useful information of this class.
+        :param data: Dictionary containing the message data.
+        :return: A dict containing useful information of this class.
+        :rtype: dict
         """
         super().parse_message(data)
 
     def EnabledAllCollider(self, enabled: bool):
         """
-        Set the collider enabled or unenabled.
+        Enable or disable all colliders.
 
-        Args:
-            active: Bool, True for enable and False for unenable.
+        :param enabled: Bool, True to enable and False to disable.
         """
         self._send_data("EnabledAllCollider", enabled)
 
-    def SetPhysicMaterial(
-        self,
-        bounciness: float,
-        dynamicFriction: float,
-        staticFriction: float,
-        frictionCombine: int,
-        bounceCombine: int,
-    ):
+    def SetPhysicMaterial(self, bounciness: float, dynamicFriction: float, staticFriction: float, frictionCombine: int, bounceCombine: int):
         """
-        Set the collider physical material.
+        Set the physical material properties for the collider.
 
-        Args:
-            bounciness (float): The coefficient of restitution or "bounciness" of the collider. It determines how much
-                kinetic energy is retained after a collision. A value of 0 means no bounce, while a value of 1 means a
-                perfect bounce.
-            dynamicFriction (float): The coefficient of friction when the collider is in motion relative to another
-                collider. It determines how much resistance there is when the collider is sliding against another surface.
-            staticFriction (float): The coefficient of friction when the collider is at rest relative to another collider.
-                It determines the resistance to initiating motion between the collider and another surface.
-            frictionCombine (int): An integer representing how friction values should be combined when multiple colliders
-                interact. It can take on values such as:
-                - 0: Average
-                - 1: Maximum
-                - 2: Minimum
-                - 3: Multiply
-                These values define how friction will be calculated when multiple colliders are in contact.
-            bounceCombine (int): An integer representing how bounce values should be combined when multiple colliders
-                interact. It can take on values such as:
-                - 0: Average
-                - 1: Maximum
-                - 2: Minimum
-                - 3: Multiply
-                These values define how bounciness will be calculated when multiple colliders are in contact.
+        :param bounciness: Float, The coefficient of restitution or "bounciness" of the collider. A value of 0 means no bounce, while a value of 1 means a perfect bounce.
+        :param dynamicFriction: Float, The coefficient of friction when the collider is in motion relative to another collider.
+        :param staticFriction: Float, The coefficient of friction when the collider is at rest relative to another collider.
+        :param frictionCombine: Int, Defines how friction values should be combined when multiple colliders interact. Possible values:
+            - 0: Average
+            - 1: Maximum
+            - 2: Minimum
+            - 3: Multiply
+        :param bounceCombine: Int, Defines how bounce values should be combined when multiple colliders interact. Possible values:
+            - 0: Average
+            - 1: Maximum
+            - 2: Minimum
+            - 3: Multiply
         """
         self._send_data(
             "SetPhysicMaterial",
@@ -71,19 +54,18 @@ class ColliderAttr(attr.GameObjectAttr):
         """
         Set the collider active or inactive in RFMove.
 
-        Args:
-            active: Bool, True for active and False for inactive.
+        :param active: Bool, True for active and False for inactive.
         """
         self._send_data("SetRFMoveColliderActive", active)
 
     def GenerateVHACDColider(self):
         """
-        Generate convex colliders using VHACD algorithm.
+        Generate convex colliders using the VHACD algorithm.
         """
         self._send_data("GenerateVHACDColider")
 
     def AddObiCollider(self):
         """
-        Add ObiCollider for this Collider.
+        Add an ObiCollider to this Collider. https://obi.virtualmethodstudio.com/manual/6.3/collisions.html
         """
         self._send_data("AddObiCollider")
