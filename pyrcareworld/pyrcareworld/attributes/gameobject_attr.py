c63dce286c8ca312066f8d382607a9c02119b192
YoruCathy <cathyye0809@gmail.com>
2024-07-31 22:54:58 -0400

    Reformatted game object attr comments.

--- pyrcareworld/pyrcareworld/attributes/gameobject_attr.py
+++ pyrcareworld/pyrcareworld/attributes/gameobject_attr.py
@@ -1,6 +1,5 @@
 import pyrcareworld.attributes as attr
 
-
 class GameObjectAttr(attr.BaseAttr):
     """
     Basic game object attribute class.
@@ -8,21 +7,21 @@ class GameObjectAttr(attr.BaseAttr):
 
     def parse_message(self, data: dict):
         """
-        Parse messages. This function is called by internal function.
+        Parse messages. This function is called by an internal function.
 
-        Returns:
-            Dict: A dict containing useful information of this class.
+        :param data: Dictionary containing the message data.
+        :return: A dict containing useful information of this class.
+        :rtype: dict
 
-            self.data['3d_bounding_box']: The 3d bounding box of objects.
+        self.data['3d_bounding_box']: The 3D bounding box of objects.
         """
         super().parse_message(data)
 
     def SetColor(self, color: list):
         """
-        Set object color.
+        Set the object color.
 
-        Args:
-            color: A list of length 4, represenging r, g, b and a. Each float is in range (0, 1).
+        :param color: A list of length 4, representing r, g, b, and a. Each float is in the range (0, 1).
         """
         if color is not None:
             assert len(color) == 4, "color length must be 4"
@@ -32,25 +31,22 @@ class GameObjectAttr(attr.BaseAttr):
 
     def EnabledRender(self, enabled: bool):
         """
-        Enable or disable rendering system.
+        Enable or disable the rendering system.
 
-        Args:
-            enabled: Bool, Ture for enable rendering and False for disable rendering.
+        :param enabled: Bool, True to enable rendering and False to disable rendering.
         """
         self._send_data("EnabledRender", enabled)
 
     def SetTexture(self, path: str):
         """
-        Set the texture of object.
+        Set the texture of the object.
 
-        Args:
-            path: Str, the absolute path for texture file.
+        :param path: Str, the absolute path for the texture file.
         """
         self._send_data("SetTexture", path)
 
     def Get3DBBox(self):
         """
-        Get the 3d bounding box of this object.
-
+        Get the 3D bounding box of this object.
         """
         self._send_data("Get3DBBox")
