1950eb1b7dc04621198cef67bf583b7c0904c741
YoruCathy <cathyye0809@gmail.com>
2024-07-31 22:53:42 -0400

    Reformatted cloth attr comments.

--- pyrcareworld/pyrcareworld/attributes/cloth_attr.py
+++ pyrcareworld/pyrcareworld/attributes/cloth_attr.py
@@ -1,41 +1,39 @@
 import pyrcareworld.attributes as attr
 
-
 class ClothAttr(attr.BaseAttr):
     """
     Obi cloth class.
     """
+
     def parse_message(self, data: dict):
         """
-        Parse messages. This function is called by internal function.
+        Parse messages. This function is called by an internal function.
 
-        Returns:
-            Dict: A dict containing useful information of this class.
-
+        :param data: Dictionary containing the message data.
+        :return: A dict containing useful information of this class.
+        :rtype: dict
         """
         super().parse_message(data)
 
     def GetParticles(self):
         """
-        get the cloth particles.
+        Get the cloth particles.
         """
         self._send_data("GetParticles")
 
     def AddAttach(self, id: int, max_dis: float = 0.03):
         """
-        Add Attach clothing to attr object with given ID
+        Add Attach clothing to attr object with the given ID.
 
-        Args:
-            id: Int, Tatget attr object id.
-            max_dis: Float, max distance.
+        :param id: Int, Target attr object ID.
+        :param max_dis: Float, max distance.
         """
         self._send_data("AddAttach", int(id), float(max_dis))
 
     def RemoveAttach(self, id: int):
         """
-        Remove Attach clothing to attr object with given ID
+        Remove Attach clothing from attr object with the given ID.
 
-        Args:
-            id: Int, Tatget attr object id.
+        :param id: Int, Target attr object ID.
         """
         self._send_data("RemoveAttach", int(id))
