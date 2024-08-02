1055fdc91ac24c39c72bf695f5594ab707740428
YoruCathy <cathyye0809@gmail.com>
2024-08-02 12:51:25 -0400

    Updated light example.

--- pyrcareworld/pyrcareworld/demo/examples/example_light.py
+++ pyrcareworld/pyrcareworld/demo/examples/example_light.py
@@ -1,25 +1,46 @@
 from pyrcareworld.envs.base_env import RCareWorld
 from pyrcareworld.attributes.light_attr import LightType
 
+# Initialize the environment with the specified scene file
 env = RCareWorld(scene_file="LightScene.json")
 
+# Get the light attribute by ID
 light = env.GetAttr(885275)
+
+# Set the shadow distance
 env.SetShadowDistance(50)
-while 1:
+
+# Main loop to change light properties
+while True:
+    # Step the environment and set light color
     env.step(50)
     light.SetColor(color=[1.0, 0.0, 0.0])
+
+    # Step the environment and set light range
     env.step(50)
     light.SetRange(30.0)
+
+    # Step the environment and set light type to Directional
     env.step(50)
     light.SetType(LightType.Directional)
+
+    # Step the environment and set light intensity
     env.step(50)
     light.SetIntensity(5.0)
+
+    # Step the environment and set light type to Spot
     env.step(50)
     light.SetType(LightType.Spot)
+
+    # Step the environment and set light spot angle
     env.step(50)
     light.SetSpotAngle(60.0)
+
+    # Step the environment and set light type to Point
     env.step(50)
     light.SetType(LightType.Point)
+
+    # Step the environment and reset light properties
     env.step(50)
     light.SetRange(10.0)
     light.SetIntensity(1.0)
