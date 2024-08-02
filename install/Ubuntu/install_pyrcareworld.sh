ae8eee9e6d34297972fe309f6122f0088efb53a0
YoruCathy <cathyye0809@gmail.com>
2024-08-01 22:38:30 -0400

    Removed deprecated install scripts.

--- install/Ubuntu/install_pyrcareworld.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash -l
-eval "$(conda shell.bash hook)"
-conda create -y --name rcareworld python=3.10 pip
-source activate rcareworld
-cd pyrcareworld
-pip install -r requirements.txt
-pip install -e .
-cd ..
