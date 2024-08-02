a051e785a389042b35bf6b96d1e267f72a85f0b4
YoruCathy <cathyye0809@gmail.com>
2024-08-02 13:30:10 -0400

    Updated stretch-3 example with refined gripper code..

--- pyrcareworld/pyrcareworld/demo/examples/example_stretch3_move.py
+++ pyrcareworld/pyrcareworld/demo/examples/example_stretch3_move.py
@@ -5,10 +5,17 @@ import pyrcareworld.attributes as attr
 env = RCareWorld(assets=["stretch-3"])
 
 # Create an instance of the Stretch robot and set its initial properties
-stretch = env.InstanceObject(name="stretch-3", attr_type=attr.ControllerAttr)
+stretch = env.InstanceObject(name="stretch-3", id=123456, attr_type=attr.ControllerAttr)
 stretch.SetTransform(position=[0, 0.05, 0])
 stretch.SetImmovable(False)
 env.step()
+gripper = env.GetAttr(1234560)
+gripper.GripperOpen()
+env.step()
+
+stretch.IKTargetDoMove(position=[0, 0.5, 0.5], duration=3, speed_based=False)
+# stretch.IKTargetDoRotate(rotation=[0, 45, 180], duration=0, speed_based=False)
+stretch.WaitDo()
 
 # Uncomment the following block to move and turn the Stretch robot with specific steps
 # stretch.MoveForward(1, 0.2)
@@ -27,8 +34,5 @@ env.step()
 # env.step(300)
 # stretch.TurnRight(90, 30)
 # env.step(300)
-
-# Main loop to continuously turn the Stretch robot left
-while True:
-    stretch.TurnLeft(90, 30)
-    env.step(300)
+stretch.TurnLeft(90, 30)
+env.step(300)
