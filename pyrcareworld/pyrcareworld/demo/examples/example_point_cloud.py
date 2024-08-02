5a6f0176f067879c70ca5f074eb1b6f36d8e193a
YoruCathy <cathyye0809@gmail.com>
2024-08-02 12:49:54 -0400

    Updated camera related examples.

--- pyrcareworld/pyrcareworld/demo/examples/example_point_cloud.py
+++ pyrcareworld/pyrcareworld/demo/examples/example_point_cloud.py
@@ -1,9 +1,12 @@
 import os
 
+# Enable OpenCV EXR support
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+
 from pyrcareworld.envs.base_env import RCareWorld
 import pyrcareworld.utils.depth_processor as dp
 
+# Ensure open3d is installed
 try:
     import open3d as o3d
 except ImportError:
@@ -11,13 +14,17 @@ except ImportError:
         "This feature requires open3d, please install with `pip install open3d`"
     )
 
+# Initialize the environment with the specified scene file
 env = RCareWorld(scene_file="PointCloud.json")
+
+# Capture data from the first camera
 camera1 = env.GetAttr(698548)
 camera1.GetDepthEXR(width=1920, height=1080)
 camera1.GetRGB(width=1920, height=1080)
 camera1.GetID(width=1920, height=1080)
 env.step()
 
+# Process the captured data from the first camera
 image_rgb = camera1.data["rgb"]
 image_depth_exr = camera1.data["depth_exr"]
 fov = 60
@@ -26,24 +33,26 @@ point1 = dp.image_bytes_to_point_cloud(
     image_rgb, image_depth_exr, fov, local_to_world_matrix
 )
 
+# Capture data from the second camera
 camera2 = env.GetAttr(698550)
 camera2.GetDepthEXR(width=1920, height=1080)
 camera2.GetRGB(width=1920, height=1080)
 camera2.GetID(width=1920, height=1080)
 env.step()
 
+# Process the captured data from the second camera
 image_rgb = camera2.data["rgb"]
 image_depth_exr = camera2.data["depth_exr"]
-fov = 60
 local_to_world_matrix = camera2.data["local_to_world_matrix"]
 point2 = dp.image_bytes_to_point_cloud(
     image_rgb, image_depth_exr, fov, local_to_world_matrix
 )
 
+# Close the environment
 env.close()
 
-# unity space to open3d space and show
+# Transform point clouds to Open3D space and visualize
 point1.transform([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 point2.transform([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-coorninate = o3d.geometry.TriangleMesh.create_coordinate_frame()
-o3d.visualization.draw_geometries([point1, point2, coorninate])
+coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+o3d.visualization.draw_geometries([point1, point2, coordinate_frame])
