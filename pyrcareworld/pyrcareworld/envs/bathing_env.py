bafe8198cd22a06a75447d9e612b3c9f026d52c4
Tom Silver <tomssilver@gmail.com>
2024-08-18 09:37:17 -0400

    typo

--- pyrcareworld/pyrcareworld/envs/bathing_env.py
+++ pyrcareworld/pyrcareworld/envs/bathing_env.py
@@ -8,7 +8,7 @@ _TEMPLATE_PATH = Path(__file__).parent.parent.parent / "template"
 _EXECUTABLE_PATH = _TEMPLATE_PATH / "Bathing" / "BathingPlayer.x86_64"
 
 
-class BathingEnvironment(RCareWorld):
+class BathingEnv(RCareWorld):
     """Bathing environment for the PhyRC competition."""
 
     def __init__(self, *args, **kwargs):
--- pyrcareworld/pyrcareworld/envs/dressing_env.py
+++ pyrcareworld/pyrcareworld/envs/dressing_env.py
@@ -8,7 +8,7 @@ _TEMPLATE_PATH = Path(__file__).parent.parent.parent / "template"
 _EXECUTABLE_PATH = _TEMPLATE_PATH / "Dressing" / "DressingPlayer.x86_64"
 
 
-class DressingEnvironment(RCareWorld):
+class DressingEnv(RCareWorld):
     """Dressing environment for the PhyRC competition."""
 
     def __init__(self, *args, **kwargs):
