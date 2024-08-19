2b137f098c44ac7c3a62296797375598d2b393c9
Tom Silver <tomssilver@gmail.com>
2024-08-19 09:54:23 -0400

    incorporate feedback

--- pyrcareworld/pyrcareworld/envs/dressing_env.py
+++ pyrcareworld/pyrcareworld/envs/dressing_env.py
@@ -5,11 +5,11 @@ from pathlib import Path
 
 
 _TEMPLATE_PATH = Path(__file__).parents[3] / "template"
-_EXECUTABLE_PATH = _TEMPLATE_PATH / "Dressing" / "DressingPlayer.x86_64"
+_DEFAULT_EXECUTABLE_PATH = _TEMPLATE_PATH / "Dressing" / "DressingPlayer.x86_64"
 
 
 class DressingEnv(RCareWorld):
     """Dressing environment for the PhyRC competition."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(executable_file=str(_EXECUTABLE_PATH), *args, **kwargs)
+    def __init__(self, executable_file=str(_DEFAULT_EXECUTABLE_PATH), *args, **kwargs):
+        super().__init__(executable_file=executable_file, *args, **kwargs)
