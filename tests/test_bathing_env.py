980e264a38f926048cc5853763eb5b62746f5ef2
Tom Silver <tomssilver@gmail.com>
2024-08-28 11:33:26 -0400

    start tests for move commands

--- tests/test_bathing_env.py
+++ tests/test_bathing_env.py
@@ -1,106 +1,71 @@
-"""Tests for the bathing environment."""
+"""Tests for the bathing environment.
+
+NOTE: run this file with pytest -s tests/test_bathing_env.py.
+"""
 
 from pyrcareworld.envs.bathing_env import BathingEnv
 
 import pytest
-import cv2
 import numpy as np
+
+
+def wrap_angle(angle):
+    """Wrap an angle in radians to [-pi, pi]."""
+    return np.arctan2(np.sin(angle), np.cos(angle))
+
+
+def get_signed_angle_distance(target, source):
+    """Given two angles between [-pi, pi], get the smallest signed angle d s.t.
+
+    source + d = target.
+    """
+    assert -np.pi <= source <= np.pi
+    assert -np.pi <= target <= np.pi
+    a = target - source
+    return (a + np.pi) % (2 * np.pi) - np.pi
+
+
+def euler_angles_allclose(euler1, euler2, atol = 1e-6):
+    """Compare two euler angles."""
+    wrapped_angles1 = [wrap_angle(a) for a in euler1]
+    wrapped_angles2 = [wrap_angle(a) for a in euler2]
+    dists = [get_signed_angle_distance(a, b)
+             for a, b in zip(wrapped_angles1, wrapped_angles2)]
+    return np.allclose(dists, 0, atol=atol)
 
 
 @pytest.fixture(scope="session", name="bathing_env", autouse=True)
 def _bathing_env_fixture():
     """Create a BathingEnv once and share it across tests."""
+    # NOTE: set graphics = True here to debug.
     env = BathingEnv(graphics=False)    
     yield env
     env.close()
 
 
-def test_bathing_env_camera(bathing_env):
-    """Tests for the camera in the bathing environment."""
+def test_bathing_stretch_move_commands(bathing_env):
+    """Tests for turning and moving the stretch base in the bathing env."""
 
-    # Extract the relevant objects.
     robot = bathing_env.get_robot()
-    gripper = bathing_env.get_gripper()
-    camera = bathing_env.get_camera()
-    sponge = bathing_env.get_sponge()
 
-    # Attach the camera to the robot's gripper.
-    camera.SetTransform(position=gripper.data['position'], rotation=[0, 0, 0])
-    camera.SetParent(gripper.id)
+    # Get the initial pose of the robot base.
+    init_robot_base_position = robot.data["position"].copy()
+    init_robot_base_rotation = robot.data["rotation"].copy()
 
-    # Move the gripper up so that the sponge can be seen.
-    robot.IKTargetDoMove(
-        position=[-0.6, 0.75, 1.25],
-        duration=2,
-        speed_based=False,
-    )
-    robot.WaitDo()
+    # This command means: turn the robot left 90 degrees with a speed of 30.
+    robot.TurnLeft(90, 30)
+    bathing_env.step(500)  # long enough for the full turn
 
-    # Test getting an RGB image.
-    img_size = 512
-    camera.GetRGB(img_size, img_size)
-    bathing_env.step()
-    rgb = np.frombuffer(camera.data["rgb"], dtype=np.uint8)
-    rgb = cv2.imdecode(rgb, cv2.IMREAD_COLOR)
-    assert rgb.shape == (img_size, img_size, 3)
-    
-    # The sponge should be in view.
-    # TODO https://github.com/empriselab/RCareWorld/discussions/103
-    # camera.Get2DBBox()
-    # bathing_env.step()
-    # bboxes2d = camera.data["2d_bounding_box"]
-    # assert sponge.id in bboxes2d
-    # bboxes3d = camera.Get3DBBox()
-    # bathing_env.step()
-    # bboxes3d = camera.data["3d_bounding_box"]
-    # assert sponge.id in bboxes3d
+    # Verify that the robot's pose has changed by the expected amount.
+    new_robot_base_position = robot.data["position"].copy()
+    new_robot_base_rotation = robot.data["rotation"].copy()
 
-    # Test GetAmodalMask() for the sponge.
-    camera.GetAmodalMask(sponge.id, img_size, img_size)
-    bathing_env.step()
-    amodal_mask = np.frombuffer(camera.data["amodal_mask"], dtype=np.uint8)
-    amodal_mask = cv2.imdecode(amodal_mask, cv2.IMREAD_COLOR)
-    import ipdb; ipdb.set_trace()
+    # The robot position is expected not to move much.
+    expected_robot_base_position = init_robot_base_position.copy()
+    assert np.allclose(new_robot_base_position, expected_robot_base_position)
 
+    # The yaw should have changed by about 90 degrees.
+    expected_robot_base_rotation = np.add(init_robot_base_rotation, (0.0, 0.0, 90.0))
+    assert euler_angles_allclose(new_robot_base_rotation, expected_robot_base_rotation)
 
-
-# def test_snippet():
-
-
-#     from pyrcareworld.envs.bathing_env import BathingEnv
-
-#     import pytest
-#     import cv2
-#     import numpy as np
-
-#     env = BathingEnv(graphics=True)
-
-#     robot = env.GetAttr(221582)
-#     gripper = env.GetAttr(2215820)
-#     sponge = env.GetAttr(91846)
-#     camera_hand = env.GetAttr(654321)
-#     camera_hand.SetTransform(position=gripper.data['position'], rotation=[0, 0, 0])
-#     camera_hand.SetParent(2215820)
-#     env.step()
-
-#     # Move the gripper up so that the sponge can be seen.
-#     robot.IKTargetDoMove(
-#         position=[-0.6, 0.75, 1.25],
-#         duration=2,
-#         speed_based=False,
-#     )
-#     robot.WaitDo()
-#     env.step()
-
-#     camera_hand.GetRGB(512, 512)
-#     env.step()
-#     rgb = np.frombuffer(camera_hand.data["rgb"], dtype=np.uint8)
-#     rgb = cv2.imdecode(rgb, cv2.IMREAD_COLOR)
-#     cv2.imwrite("rgb_hand.png", rgb)
-
-#     # The sponge should be in view.
-#     env.step()
-#     camera_hand.Get2DBBox()
-#     env.step()
-#     bboxes2d = camera_hand.data["2d_bounding_box"]
-#     assert sponge.id in bboxes2d
+    # TODO write analogous tests for robot.TurnRight, robot.MoveForward, robot.MoveBack.
