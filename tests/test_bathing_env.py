3b9e23649eebad79bf2cd59f1b0b4db31beabbc2
thisjustin123 <jjg283@cornell.edu>
2024-10-20 21:53:26 -0400

    Adjust right turn rate

--- tests/test_bathing_env.py
+++ tests/test_bathing_env.py
@@ -143,7 +143,7 @@ def test_target_angle_drive(bathing_env: BathingEnv):
     # Drive right by setting left and right velocities.
     robot_base_rotation = robot.data["rotations"][0].copy()
     for _ in range(num_steps_per_command):
-        robot.TargetVelocity(0.25, -0.25)
+        robot.TargetVelocity(0.5, -0.5)
         bathing_env.step()
 
     new_robot_base_rotation = robot.data["rotations"][0].copy()
