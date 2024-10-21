c79949554040e5ffeb149ae0b29a190f6c26a7aa
thisjustin123 <jjg283@cornell.edu>
2024-10-20 20:50:06 -0400

    See what happens with higher velocity

--- tests/test_bathing_env.py
+++ tests/test_bathing_env.py
@@ -111,7 +111,7 @@ def test_target_angle_drive(bathing_env: BathingEnv):
     # Drive forward by setting left and right velocities.
     robot_base_position = robot.data["positions"][0].copy()
     for _ in range(num_steps_per_command):
-        robot.TargetVelocity(0.1, 0.1)
+        robot.TargetVelocity(0.5, 0.5)
         bathing_env.step()
 
     new_robot_base_position = robot.data["positions"][0].copy()
